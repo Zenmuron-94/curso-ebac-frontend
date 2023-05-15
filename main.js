06a2a75f6fd782ec9e03987c0aaf67381c45f6f4
Zenmuron-94 <bioclp@hotmail.com>
2023-05-15 04:12:43 -0300

    initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+$('#telefone').mask('(00)00000-0000')
+$('#cpf').mask('000.000.000/00')
+$('#cep').mask('00000-000')
+$('#numeroCasa').mask('00000')
+$('#numeroApto').mask('00000')
+
+
+})
